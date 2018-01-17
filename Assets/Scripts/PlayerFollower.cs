490cb1eb38a88b07763a74182d60ac0e48fb2674
Todor Bochukov <todor@chobolabs.com>
2018-01-17 18:19:41 +0200

    Unity project.
    Simple top-down character movement and look.

---
+++ Assets/Scripts/PlayerFollower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollower : MonoBehaviour
+{
+    public Rigidbody2D playerRigidbody2D;
+    public float distance;
+    public float smoothTime;
+
+    private Vector2 currentVelocity;
+
+    void FixedUpdate()
+    {
+        var direction = playerRigidbody2D.velocity.normalized;
+        var targetPosition = playerRigidbody2D.position + direction * distance;
+
+        transform.position = new Vector3(
+            Mathf.SmoothDamp(transform.position.x, targetPosition.x, ref currentVelocity.x, smoothTime),
+            Mathf.SmoothDamp(transform.position.y, targetPosition.y, ref currentVelocity.y, smoothTime),
+            transform.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    private new Rigidbody2D rigidbody2D;
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rigidbody2D.velocity = direction * speed;
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        var look = GetMouseWorldPosition() - rigidbody2D.position;
+        rigidbody2D.rotation = Mathf.Rad2Deg * Mathf.Atan2(look.y, look.x);
+    }
+
+    private Vector2 GetMouseWorldPosition()
+    {
+        var camera = Camera.main;
+
+        if (camera.orthographic)
+        {
+            return camera.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        var ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        float distance;
+        if (new Plane(Vector3.back, 0).Raycast(ray, out distance))
+            return ray.GetPoint(distance);
+
+        return new Vector2();
+    }
+}
