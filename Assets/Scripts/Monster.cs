df11f90df7c33ee01eb54c02c5190fe7997e69be
Todor Bochukov <todor@chobolabs.com>
2018-01-18 12:32:15 +0200

    Added monsters that walk toward the player.

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public float speed;
+
+    private Player player;
+    private Rigidbody2D body;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        Debug.Assert(player != null);
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = (player.Body.position - body.position).normalized * speed;
+    }
+}
