c10bb9a04f972c281923ac6b84ed17570f64b7ee
Todor Bochukov <todor@chobolabs.com>
2018-01-31 18:55:38 +0200

    Weapons v.2 WIP.

---
+++ Assets/Scripts/Boomerang.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Boomerang : MonoBehaviour
+{
+    public Vector2 force;
+
+    public Rigidbody2D Body { get { return body; } }
+
+    private Rigidbody2D body;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+
+        body.AddRelativeForce(new Vector2(10, 0), ForceMode2D.Impulse);
+        // body.AddTorque(-1, ForceMode2D.Impulse);
+    }
+
+    private void FixedUpdate()
+    {
+        body.AddRelativeForce(new Vector2(0, -0.1f), ForceMode2D.Impulse);
+        //body.AddRelativeForce(new Vector2(10, 0), ForceMode2D.Impulse);
+        //body.AddTorque(100, ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Projectile : MonoBehaviour
+{
+    public WeaponType type;
+
+    public Rigidbody2D Body { get { return body; } }
+
+    private Rigidbody2D body;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (body.IsSleeping())
+        {
+            var control = GetComponentInParent<GameControl>();
+            Instantiate(type.pickable, transform.position, transform.rotation, control.transform);
+
+            Destroy(gameObject);
+        }
+    }
+}
