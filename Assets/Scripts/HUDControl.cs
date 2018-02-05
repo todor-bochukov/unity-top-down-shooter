0c0acbb770b039f6367631b9d63dfed185091c00
Todor Bochukov <todor@chobolabs.com>
2018-02-05 17:36:45 +0200

    Added kill count.

---
+++ Assets/Scripts/HUDControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDControl : MonoBehaviour
+{
+    public Animator animator;
+    public Text killsText;
+
+    void Start()
+    {
+        SetKills(0);
+    }
+
+    public void SetKills(uint kills)
+    {
+        killsText.text = kills.ToString();
+        if (kills > 0)
+        {
+            animator.SetTrigger("Kill");
+        }
+    }
+}
