1f4c8171de37ade3b3795b3b980dd4ccf2a57d7b
Todor Bochukov <todor@chobolabs.com>
2018-02-12 19:42:40 +0200

    Fixed a bug where toggles wouldn't animate to correct state after their parent object gets activated for a second time.

--- Assets/Scripts/ToggleAnimator.cs
+++ Assets/Scripts/ToggleAnimator.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Toggle))]
 public class ToggleAnimator : MonoBehaviour
 {
     public Animator animator;
     public string boolParameterName;
+
+    private Toggle toggle;
+
+    private void Awake()
+    {
+        toggle = GetComponent<Toggle>();
+    }
+
+    public void OnEnable()
+    {
+        animator.SetBool(boolParameterName, toggle.isOn);
+    }
 
     public void OnToggleValueChanged(bool isOn)
     {
