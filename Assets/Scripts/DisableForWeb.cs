2949c5511684eb36394a389ebb8d327bdbb99037
Todor Bochukov <todor@chobolabs.com>
2018-02-21 14:42:51 +0200

    Disabled exit and graphics settings for browser build.

---
+++ Assets/Scripts/DisableForWeb.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableForWeb : MonoBehaviour
+{
+# if UNITY_WEBGL
+    private void Awake()
+    {
+        gameObject.SetActive(false);
+    }
+#endif // UNITY_WEBGL
+}
