11132147b0104bca7136050c5b65b5d09f6678ca
Todor Bochukov <todor@chobolabs.com>
2018-02-22 18:24:30 +0200

    Added music and sound volume options. Added temp background music.

---
+++ Assets/Scripts/VolumeOptions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class VolumeOptions : MonoBehaviour
+{
+    public AudioControl audioControl;
+    public Slider musicVolume;
+    public Slider soundVolume;
+
+    private void Start()
+    {
+        musicVolume.onValueChanged.AddListener(CreateSetVolumeFn(AudioControl.musicVolumeKey));
+        musicVolume.value = audioControl.GetMusicVolume();
+
+        soundVolume.onValueChanged.AddListener(CreateSetVolumeFn(AudioControl.soundVolumeKey));
+        soundVolume.value = audioControl.GetSoundVolume();
+    }
+
+    private UnityAction<float> CreateSetVolumeFn(string key)
+    {
+        return value => audioControl.SetVolume(key, value);
+    }
+}
