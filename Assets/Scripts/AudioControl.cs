87370e05134271b217e8ac4526731929db9e9078
Todor Bochukov <todor@chobolabs.com>
2018-02-22 18:24:30 +0200

    Added music and sound volume options. Added temp background music.

---
+++ Assets/Scripts/AudioControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioControl : MonoBehaviour
+{
+    public const string musicVolumeKey = "Music";
+    public const string soundVolumeKey = "Sound";
+
+    public float musicVolumeDefault;
+    public float soundVolumeDefault;
+
+    void Awake()
+    {
+        SetVolume(musicVolumeKey, GetMusicVolume());
+        SetVolume(soundVolumeKey, GetSoundVolume());
+    }
+
+    public float GetMusicVolume()
+    {
+        return PlayerPrefs.GetFloat(musicVolumeKey, musicVolumeDefault);
+    }
+
+    public float GetSoundVolume()
+    {
+        return PlayerPrefs.GetFloat(soundVolumeKey, soundVolumeDefault);
+    }
+
+    public void SetVolume(string key, float value)
+    {
+        foreach (var go in GameObject.FindGameObjectsWithTag(key))
+        {
+            foreach (var component in go.GetComponentsInChildren<AudioSource>())
+            {
+                component.volume = value;
+            }
+        }
+
+        PlayerPrefs.SetFloat(key, value);
+    }
+}
---
+++ Assets/Scripts/SliderValue.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class SliderValue : MonoBehaviour
+{
+    public Slider slider;
+    public Text text;
+
+    public bool wholeNumbers;
+    public float minValue;
+    public float maxValue;
+
+    private void Start()
+    {
+        UnityAction<float> setValueToText = value => text.text = Convert(value).ToString();
+
+        setValueToText(slider.value);
+
+        slider.onValueChanged.AddListener(setValueToText);
+    }
+
+    private float Convert(float value)
+    {
+        Debug.Assert(slider.maxValue != slider.minValue);
+
+        var sliderRange = slider.maxValue - slider.minValue;
+        var range = maxValue - minValue;
+
+        value = minValue + range * (value - slider.minValue) / sliderRange;
+
+        if (wholeNumbers)
+            value = Mathf.Round(value);
+
+        return value;
+    }
+}
